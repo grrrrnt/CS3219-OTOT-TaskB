282a7995759d5baa0828988b0896564097cda05b
grrrrnt <grantlee97@gmail.com>
2020-11-15 19:59:47 +0800

    Add React frontend SPA

--- remindme/src/App.js
+++ remindme/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
     state = {
         response: '',
-        post: '',
         responseToPost: '',
+        responseToGet: '',
+        responseToUpdate: '',
+        responseToDelete: '',
+        reminder: 'e.g. Wash my hands',
+        details: 'Otherwise I\'ll catch COVID',
+        remind_when: 'Every day',
+        id: ''
     };
 
     componentDidMount() {
@@ -16,24 +21,81 @@ class App extends Component {
     }
 
     callApi = async () => {
-        const response = await fetch('/api/hello');
+        const response = await fetch('/api/reminders');
         const body = await response.json();
         if (response.status !== 200) throw Error(body.message);
         return body;
     };
 
-    handleSubmit = async e => {
+    handleCreateSubmit = async e => {
         e.preventDefault();
-        const response = await fetch('/api/world', {
+        const response = await fetch('/api/reminders', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ post: this.state.post }),
+            body: JSON.stringify({
+                reminder: this.state.reminder,
+                details: this.state.details,
+                remind_when: this.state.remind_when
+            }),
         });
         const body = await response.text();
         this.setState({ responseToPost: body });
     };
+
+    handleGetSubmit = async e => {
+        e.preventDefault();
+        if (this.state.id === '') {
+            const response = await fetch('/api/reminders', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+            const body = await response.text();
+            this.setState({ responseToGet: body });
+        } else {
+            const response = await fetch('/api/reminders/' + this.state.id, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+            const body = await response.text();
+            this.setState({ responseToGet: body });
+        }
+    };
+
+    handleUpdateSubmit = async e => {
+        e.preventDefault();
+        const response = await fetch('/api/reminders/' + this.state.id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                reminder: this.state.reminder,
+                details: this.state.details,
+                remind_when: this.state.remind_when
+            }),
+        });
+        const body = await response.text();
+        this.setState({ responseToUpdate: body });
+    };
+
+    handleDeleteSubmit = async e => {
+        e.preventDefault();
+        const response = await fetch('/api/reminders/' + this.state.id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        const body = await response.text();
+        this.setState({ responseToDelete: body });
+    };
+
     render() {
         return (
             <div className="App">
@@ -43,20 +105,95 @@ class App extends Component {
                 </p>
               </header>
               <p>{this.state.response}</p>
-              <form onSubmit={this.handleSubmit}>
+              <form onSubmit={this.handleCreateSubmit}>
                 <p>
-                  <strong>Remind myself to:</strong>
+                  <strong>Create new reminder:</strong>
                 </p>
+                <p>Reminder: </p>
                 <input
                     type="text"
-                    value={this.state.post}
-                    onChange={e => this.setState({ post: e.target.value })}
+                    value={this.state.reminder}
+                    onChange={e => this.setState({ reminder: e.target.value })}
                 />
+                <p>Details: </p>
+                <input
+                    type="text"
+                    value={this.state.details}
+                    onChange={e => this.setState({ details: e.target.value })}
+                />
+                <p>Remind when: </p>
+                <input
+                    type="text"
+                    value={this.state.remind_when}
+                    onChange={e => this.setState({ remind_when: e.target.value })}
+                />
+                <p></p>
                 <button type="submit">Set reminder</button>
               </form>
               <p>{this.state.responseToPost}</p>
+                <form onSubmit={this.handleGetSubmit}>
+                    <p>
+                        <strong>Get reminder:</strong>
+                    </p>
+                    <p>ID (if left blank, it will retrieve all): </p>
+                    <input
+                        type="text"
+                        value={this.state.id}
+                        onChange={e => this.setState({ id: e.target.value })}
+                    />
+                    <p></p>
+                    <button type="submit">Get reminder(s)</button>
+                </form>
+                <p>{this.state.responseToGet}</p>
+                <form onSubmit={this.handleUpdateSubmit}>
+                    <p>
+                        <strong>Update existing reminder:</strong>
+                    </p>
+                    <p>ID: </p>
+                    <input
+                        type="text"
+                        value={this.state.id}
+                        onChange={e => this.setState({ id: e.target.value })}
+                    />
+                    <p>Reminder: </p>
+                    <input
+                        type="text"
+                        value={this.state.reminder}
+                        onChange={e => this.setState({ reminder: e.target.value })}
+                    />
+                    <p>Details: </p>
+                    <input
+                        type="text"
+                        value={this.state.details}
+                        onChange={e => this.setState({ details: e.target.value })}
+                    />
+                    <p>Remind when: </p>
+                    <input
+                        type="text"
+                        value={this.state.remind_when}
+                        onChange={e => this.setState({ remind_when: e.target.value })}
+                    />
+                    <p></p>
+                    <button type="submit">Update reminder</button>
+                </form>
+                <p>{this.state.responseToUpdate}</p>
+                <form onSubmit={this.handleDeleteSubmit}>
+                    <p>
+                        <strong>Delete reminder:</strong>
+                    </p>
+                    <p>ID: </p>
+                    <input
+                        type="text"
+                        value={this.state.id}
+                        onChange={e => this.setState({ id: e.target.value })}
+                    />
+                    <p></p>
+                    <button type="submit">Delete reminder</button>
+                </form>
+                <p>{this.state.responseToDelete}</p>
             </div>
       );
     }
 }
+
 export default App;
